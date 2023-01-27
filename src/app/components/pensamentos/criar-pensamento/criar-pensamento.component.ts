b45f6c29d19248afa8ec8418088043940e9f0a90
Victor <paulo9.victor.silva@gmail.com>
2023-01-27 14:54:07 -0300

    first commit

---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.scss']
+})
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamentos = {
+    conteudo: 'Aprendendo Angular',
+    autoria: 'dev'
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  criarPensamento() {
+    alert('Pensamento criado')
+  }
+
+  cancelarPensamento() {
+    alert('Pensamento cancelado')
+  }
+
+}
